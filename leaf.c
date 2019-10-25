f9360f1947290e287879dca3026365fd0ed5e8a1
SuperLeaf1995 <jesusantonio30122016@gmail.com>
2019-10-25 12:50:30 -0500

    Merged key.h, merged functions, deleted bullet physics

--- leaf.c
+++ leaf.c
@@ -2,8 +2,6 @@
 @Action: Sets video to *video*
 @Parameters: video=16-bit long value of the desired video mode
 @Output: Old video mode
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT+WIP)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*T), EGA (*T), CGA (*T)
 */
 int16_t _Cdecl setVideo(register int16_t video) { /*Sets the video using int 10h*/
 	static int16_t oldVideo;
@@ -21,6 +19,203 @@ int16_t _Cdecl setVideo(register int16_t video) { /*Sets the video using int 10h
     in.h.al = video;
     int86(0x10,&in,&out);
     return oldVideo;
+}
+/*
+@Action: Reads bitmap file header (before information)
+@Parameters: stream=file stream. structure bitmapFileHeader=structure pointer, we will write data there!
+@Output: void, but overwrites structure bitmapFileHeader
+*/
+void _Cdecl readBitmapHeader(FILE *stream, struct bitmapFileHeader *b, struct bitmapInfoHeader *e) {
+	static uint32_t headerSize;
+    static uint32_t wide;
+    static uint32_t tall;
+    static uint16_t planes;
+    static uint16_t bitsPerPixel;
+    static uint32_t compression;
+    static uint32_t sizeOfImage;
+    static uint32_t xPixelsPerMeter;
+    static uint32_t yPixelsPerMeter;
+    static uint32_t numberOfColors;
+    static uint32_t importantColors;
+    static uint32_t sizeOfFile,reserved,offset;
+    /*Read file header*/
+    fread(b->type,sizeof(uint16_t),1,stream);
+    fread(&sizeOfFile,sizeof(uint32_t),1,stream);
+    fread(&reserved,sizeof(uint32_t),1,stream);
+    fread(&offset,sizeof(uint32_t),1,stream);
+    b->sizeOfFile = sizeOfFile;
+    b->reserved = reserved;
+    b->offset = offset;
+    /*Now information*/
+    fread(&headerSize,sizeof(uint32_t),1,stream);
+    fread(&wide,sizeof(int32_t),1,stream);
+    fread(&tall,sizeof(int32_t),1,stream);
+    fread(&planes,sizeof(uint16_t),1,stream);
+    fread(&bitsPerPixel,sizeof(uint32_t),1,stream);
+    fread(&compression,sizeof(uint32_t),1,stream);
+    fread(&sizeOfImage,sizeof(uint32_t),1,stream);
+    fread(&xPixelsPerMeter,sizeof(uint32_t),1,stream);
+    fread(&yPixelsPerMeter,sizeof(uint32_t),1,stream);
+    fread(&numberOfColors,sizeof(uint32_t),1,stream);
+    fread(&importantColors,sizeof(uint32_t),1,stream);
+    e->headerSize = headerSize;
+    e->wide = wide;
+    e->tall = tall;
+    e->planes = planes;
+    e->bitsPerPixel = bitsPerPixel;
+    e->compression = compression;
+    e->sizeOfImage = sizeOfImage;
+    e->xPixelsPerMeter = xPixelsPerMeter;
+    e->yPixelsPerMeter = yPixelsPerMeter;
+    e->numberOfColors = numberOfColors;
+    e->importantColors = importantColors;
+    return;
+}
+
+/*
+@Action: Reads the image of a bitmap and then outputs it
+@Parameters: stream=file stream. wide=wide of image. tall=tall of image.
+@Output: data pointer
+*/
+uint8_t * _Cdecl readBitmapData(FILE *stream, uint32_t wide, uint32_t tall) {
+    static uint32_t i;
+    static uint32_t i2;
+    static uint16_t hold;
+    uint8_t *data;
+    fpos_t pos;
+    if(tall == 0 || wide == 0) {
+        return 0;
+    }
+    data = (uint8_t *)malloc(wide*tall);
+    if(data == NULL) {
+        return 0; /*Up to caller's, how to handle errors*/
+    }
+    fgetpos(stream,&pos);
+    for(i = 1; i < tall+1; i++) { /*Reverse scan, reverse tall, but not wide*/
+		for(i2 = 0; i2 < wide; i2++) {
+			fread(&hold,sizeof(uint8_t),1,stream);
+			data[(i2+((tall-i)*wide))] = hold;
+		}
+    }
+    return (uint8_t *)data;
+}
+
+/*
+@Action: Displays image
+@Parameters: data=data pointer. x=x pos. y=y pos. wide=wide of image. tall=tall of image
+@Output: void
+*/
+void _Cdecl displayImage(uint8_t *data, uint32_t x, uint32_t y, uint32_t wide, uint32_t tall) {
+    static uint32_t i;
+    static uint32_t i2;
+    for(i2 = 0; i2 < tall; i2++) {
+        for(i = 0; i < wide; i++) {
+            plotPixel(x+i,y+i2,data[i+(i2*wide)]);
+        }
+    }
+    return;
+}
+
+/*
+@Action: Displays image while reading file
+@Parameters: x=x pos. y=y pos. wide=wide of image. tall=tall of image
+@Output: void
+*/
+void _Cdecl displayImageWhileReading(FILE *stream, uint32_t x, uint32_t y, uint32_t wide, uint32_t tall) {
+    static uint32_t i;
+    static uint32_t i2;
+    static uint16_t hold;
+    for(i = tall; i > 0; i--) { /*Reverse scan, reverse tall, but not wide*/
+		for(i2 = 0; i2 < wide; i2++) {
+			fread(&hold,sizeof(uint8_t),1,stream);
+			/*(i2+((tall-i)*wide))*/
+			plotPixel(x+i2,y+i,hold);
+		}
+    }
+    return;
+}
+
+/*
+@Action: Displays image in tiled mode
+@Parameters: data=data pointer. x=x pos. y=y pos. wide=wide of tile. tall=tall of tile. index=tile number
+@Output: void
+*/
+void _Cdecl displayImageTile(uint8_t *data, uint32_t x, uint32_t y, uint32_t wide, uint32_t tall, uint32_t index) {
+    static uint32_t i;
+    static uint32_t i2;
+    for(i2 = 0; i2 < tall; i2++) {
+        for(i = 0; i < wide; i++) {
+			if(data[(index*(wide*tall))+i+(i2*wide)] != 0x24) {
+				plotPixel(x+i,y+i2,data[(index*(wide*tall))+i+(i2*wide)]);
+			}
+        }
+    }
+    return;
+}
+
+/*
+@Action: Saves bitmap
+@Parameters: stream=file stream. bfh=bitmap file header pointer. bih=bitmap info header pointer.
+data=data pointer
+@Output: void
+*/
+void _Cdecl writeBitmap(FILE *stream, struct bitmapFileHeader *bfh, struct bitmapInfoHeader *bih, uint8_t *data) {
+	static uint32_t sizeOfFile,reserved,offset; /*We do the same as in our read routines*/
+    static uint32_t headerSize;					/*we just switch the order of stuff and*/
+    static uint32_t wide;						/*change read with write.*/
+    static uint32_t tall;
+    static uint16_t planes;
+    static uint16_t bitsPerPixel;
+    static uint32_t compression;
+    static uint32_t sizeOfImage;
+    static uint32_t xPixelsPerMeter;
+    static uint32_t yPixelsPerMeter;
+    static uint32_t numberOfColors;
+    static uint32_t importantColors;
+    static uint32_t i;
+    static uint32_t i2;
+    static uint16_t hold;
+	sizeOfFile = bfh->sizeOfFile;
+    reserved = bfh->reserved;
+    offset = bfh->offset;
+    headerSize = bih->headerSize;
+    wide = bih->wide;
+    tall = bih->tall;
+    planes = bih->planes;
+    bitsPerPixel = bih->bitsPerPixel;
+    compression = bih->compression;
+    sizeOfImage = bih->sizeOfImage;
+    xPixelsPerMeter = bih->xPixelsPerMeter;
+    yPixelsPerMeter = bih->yPixelsPerMeter;
+    numberOfColors = bih->numberOfColors;
+    importantColors = bih->importantColors;
+    fwrite(bfh->type,sizeof(uint16_t),1,stream);
+    fwrite(&sizeOfFile,sizeof(uint32_t),1,stream);
+    fwrite(&reserved,sizeof(uint32_t),1,stream);
+    fwrite(&offset,sizeof(uint32_t),1,stream);
+	fwrite(&headerSize,sizeof(uint32_t),1,stream);
+    fwrite(&wide,sizeof(int32_t),1,stream);
+    fwrite(&tall,sizeof(int32_t),1,stream);
+    fwrite(&planes,sizeof(uint16_t),1,stream);
+    fwrite(&bitsPerPixel,sizeof(uint32_t),1,stream);
+    fwrite(&compression,sizeof(uint32_t),1,stream);
+    fwrite(&sizeOfImage,sizeof(uint32_t),1,stream);
+    fwrite(&xPixelsPerMeter,sizeof(uint32_t),1,stream);
+    fwrite(&yPixelsPerMeter,sizeof(uint32_t),1,stream);
+    fwrite(&numberOfColors,sizeof(uint32_t),1,stream);
+    fwrite(&importantColors,sizeof(uint32_t),1,stream);
+    /*Palette*/
+    for(i = 0; i < 1022; i++) {
+		fwrite(&i,sizeof(uint8_t),1,stream);
+	}
+	/*Bitmap data*/
+	for(i = 1; i < tall+1; i++) { /*Reverse scan, reverse tall, but not wide*/
+		for(i2 = 0; i2 < wide; i2++) { /*We just replotting our shit back again!*/
+			hold = data[(i2+((tall-i)*wide))];
+			fwrite(&hold,sizeof(uint8_t),1,stream);
+		}
+    }
+	return;
 }
 
 /*
@@ -45,8 +240,6 @@ int16_t _Cdecl setVideo(register int16_t video) { /*Sets the video using int 10h
 -16 Genoa 6100
 -17 Genoa 5100/5200
 -18 Genoa 5300/5400
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT+WIP)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*T), EGA (*T), CGA (*T)
 */
 int32_t _Cdecl getVideoAdapter(void) {
 	static uint8_t *ptr;
@@ -139,8 +332,6 @@ int32_t _Cdecl getVideoAdapter(void) {
 @Action: Plots a line
 @Parameters: fx=from x. fy=from y. tx=to x. ty=to y. color=color (byte)
 @Output: void
-@Compatible platforms: All that plotPixel() can handle
-@Compatible video modes: All that plotPixel() can handle
 */
 void _Cdecl plotLine(int16_t fx, int16_t fy, int16_t tx, int16_t ty, uint8_t color) {
 	static int16_t dx,dy,px,py,i;
@@ -182,8 +373,6 @@ void _Cdecl plotLine(int16_t fx, int16_t fy, int16_t tx, int16_t ty, uint8_t col
 because this structure will hold all mouses. And i don't think anyone uses more than 2
 bloody mouses.
 @Output: void, but returns overwritten structure *m
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*NDFI)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*UT), EGA (*UT), CGA (*UT)
 */
 char _Cdecl initMouse(struct mouse *m) {
 	in.x.ax = 0x00;
@@ -205,8 +394,6 @@ char _Cdecl initMouse(struct mouse *m) {
 @Action: Sets position of mouse
 @Parameters: x=x position. y=y position
 @Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*NDFI)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*UT), EGA (*UT), CGA (*UT)
 */
 void _Cdecl setMousePosition(uint16_t x,uint16_t y) {
 	in.x.ax = 0x04;
@@ -220,8 +407,6 @@ void _Cdecl setMousePosition(uint16_t x,uint16_t y) {
 @Action: Shows mouse
 @Parameters: void
 @Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*NDFI)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*UT), EGA (*UT), CGA (*UT)
 */
 void _Cdecl showMouse(void) {
 	in.x.ax = 0x01;
@@ -233,8 +418,6 @@ void _Cdecl showMouse(void) {
 @Action: Hide mouse
 @Parameters: void
 @Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*NDFI)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*UT), EGA (*UT), CGA (*UT)
 */
 void _Cdecl hideMouse(void) {
 	in.x.ax = 0x02;
@@ -246,8 +429,6 @@ void _Cdecl hideMouse(void) {
 @Action: Get mouse status
 @Parameters: structure mouse=structure of the mouse to write to
 @Output: void, but returns overwritten structure *m
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*NDFI)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*UT), EGA (*UT), CGA (*UT)
 */
 void _Cdecl getMouseStatus(struct mouse *m) {
 	in.x.ax = 0x03;
@@ -264,8 +445,6 @@ void _Cdecl getMouseStatus(struct mouse *m) {
 @Action: "redraws" black area left by initialized mouse once a thing has been draw
 @Parameters: structure mouse=structure of the mouse
 @Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*NDFI)
-@Compatible video modes: VGA (*T), SVGA (*T), Hercules (*UT), EGA (*UT), CGA (*UT)
 */
 void _Cdecl redrawOnMouse(struct mouse *m) {
     static uint16_t i,i2;
@@ -281,7 +460,6 @@ void _Cdecl redrawOnMouse(struct mouse *m) {
 @Action: Skips *n* number of chars
 @Parameters: stream=file stream. n=number of chars to skip
 @Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
 */
 void _Cdecl fskip(FILE *stream, uint64_t n) {
     static uint64_t i;
@@ -289,196 +467,4 @@ void _Cdecl fskip(FILE *stream, uint64_t n) {
         fgetc(stream); /*Skip characters*/
     }
     return;
-}
-
-/*
-@Action: Reads bitmap file header (before information)
-@Parameters: stream=file stream. structure bitmapFileHeader=structure pointer, we will write data there!
-@Output: void, but overwrites structure bitmapFileHeader
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
-*/
-void _Cdecl readBitmapFileHeader(FILE *stream, struct bitmapFileHeader *b) {
-    static uint32_t sizeOfFile,reserved,offset;
-    fread(b->type,sizeof(uint16_t),1,stream);
-    fread(&sizeOfFile,sizeof(uint32_t),1,stream);
-    fread(&reserved,sizeof(uint32_t),1,stream);
-    fread(&offset,sizeof(uint32_t),1,stream);
-    b->sizeOfFile = sizeOfFile;
-    b->reserved = reserved;
-    b->offset = offset;
-    return;
-}
-
-/*
-@Action: Reads bitmap information
-@Parameters: stream=file stream. structure bitmapInfoHeader=structure pointer, we will write data there!
-@Output: void, but overwrites bitmapInfoHeader
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
-*/
-void _Cdecl readBitmapInformationHeader(FILE *stream, struct bitmapInfoHeader *b) {
-    static uint32_t headerSize;
-    static uint32_t wide;
-    static uint32_t tall;
-    static uint16_t planes;
-    static uint16_t bitsPerPixel;
-    static uint32_t compression;
-    static uint32_t sizeOfImage;
-    static uint32_t xPixelsPerMeter;
-    static uint32_t yPixelsPerMeter;
-    static uint32_t numberOfColors;
-    static uint32_t importantColors;
-    fread(&headerSize,sizeof(uint32_t),1,stream);
-    fread(&wide,sizeof(int32_t),1,stream);
-    fread(&tall,sizeof(int32_t),1,stream);
-    fread(&planes,sizeof(uint16_t),1,stream);
-    fread(&bitsPerPixel,sizeof(uint32_t),1,stream);
-    fread(&compression,sizeof(uint32_t),1,stream);
-    fread(&sizeOfImage,sizeof(uint32_t),1,stream);
-    fread(&xPixelsPerMeter,sizeof(uint32_t),1,stream);
-    fread(&yPixelsPerMeter,sizeof(uint32_t),1,stream);
-    fread(&numberOfColors,sizeof(uint32_t),1,stream);
-    fread(&importantColors,sizeof(uint32_t),1,stream);
-    b->headerSize = headerSize;
-    b->wide = wide;
-    b->tall = tall;
-    b->planes = planes;
-    b->bitsPerPixel = bitsPerPixel;
-    b->compression = compression;
-    b->sizeOfImage = sizeOfImage;
-    b->xPixelsPerMeter = xPixelsPerMeter;
-    b->yPixelsPerMeter = yPixelsPerMeter;
-    b->numberOfColors = numberOfColors;
-    b->importantColors = importantColors;
-    return;
-}
-
-/*
-@Action: Reads the image of a bitmap and then outputs it
-@Parameters: stream=file stream. wide=wide of image. tall=tall of image.
-@Output: data pointer
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
-*/
-uint8_t * _Cdecl readBitmapData(FILE *stream, uint32_t wide, uint32_t tall) {
-    static uint32_t i;
-    static uint32_t i2;
-    static uint16_t hold;
-    uint8_t *data;
-    fpos_t pos;
-    if(tall == 0 || wide == 0) {
-        return 0;
-    }
-    data = (uint8_t *)malloc(wide*tall);
-    if(data == NULL) {
-        return 0; /*Up to caller's, how to handle errors*/
-    }
-    fgetpos(stream,&pos);
-    for(i = 1; i < tall+1; i++) { /*Reverse scan, reverse tall, but not wide*/
-		for(i2 = 0; i2 < wide; i2++) {
-			fread(&hold,sizeof(uint8_t),1,stream);
-			data[(i2+((tall-i)*wide))] = hold;
-		}
-    }
-    return (uint8_t *)data;
-}
-
-/*
-@Action: Displays image
-@Parameters: data=data pointer. x=x pos. y=y pos. wide=wide of image. tall=tall of image
-@Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
-*/
-void _Cdecl displayImage(uint8_t *data, uint32_t x, uint32_t y, uint32_t wide, uint32_t tall) {
-    static uint32_t i;
-    static uint32_t i2;
-    for(i2 = 0; i2 < tall; i2++) {
-        for(i = 0; i < wide; i++) {
-            plotPixel(x+i,y+i2,data[i+(i2*wide)]);
-        }
-    }
-    return;
-}
-
-/*
-@Action: Displays image in tiled mode
-@Parameters: data=data pointer. x=x pos. y=y pos. wide=wide of tile. tall=tall of tile. index=tile number
-@Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
-*/
-void _Cdecl displayImageTile(uint8_t *data, uint32_t x, uint32_t y, uint32_t wide, uint32_t tall, uint32_t index) {
-    static uint32_t i;
-    static uint32_t i2;
-    for(i2 = 0; i2 < tall; i2++) {
-        for(i = 0; i < wide; i++) {
-			if(data[(index*(wide*tall))+i+(i2*wide)] != 0x24) {
-				plotPixel(x+i,y+i2,data[(index*(wide*tall))+i+(i2*wide)]);
-			}
-        }
-    }
-    return;
-}
-
-/*
-@Action: Saves bitmap
-@Parameters: stream=file stream. bfh=bitmap file header pointer. bih=bitmap info header pointer.
-data=data pointer
-@Output: void
-@Compatible platforms: MSDOS (*T), FreeDOS (*UT), AppleII+ (*UT)
-*/
-void _Cdecl writeBitmap(FILE *stream, struct bitmapFileHeader *bfh, struct bitmapInfoHeader *bih, uint8_t *data) {
-	static uint32_t sizeOfFile,reserved,offset; /*We do the same as in our read routines*/
-    static uint32_t headerSize;					/*we just switch the order of stuff and*/
-    static uint32_t wide;						/*change read with write.*/
-    static uint32_t tall;
-    static uint16_t planes;
-    static uint16_t bitsPerPixel;
-    static uint32_t compression;
-    static uint32_t sizeOfImage;
-    static uint32_t xPixelsPerMeter;
-    static uint32_t yPixelsPerMeter;
-    static uint32_t numberOfColors;
-    static uint32_t importantColors;
-    static uint32_t i;
-    static uint32_t i2;
-    static uint16_t hold;
-	sizeOfFile = bfh->sizeOfFile;
-    reserved = bfh->reserved;
-    offset = bfh->offset;
-    headerSize = bih->headerSize;
-    wide = bih->wide;
-    tall = bih->tall;
-    planes = bih->planes;
-    bitsPerPixel = bih->bitsPerPixel;
-    compression = bih->compression;
-    sizeOfImage = bih->sizeOfImage;
-    xPixelsPerMeter = bih->xPixelsPerMeter;
-    yPixelsPerMeter = bih->yPixelsPerMeter;
-    numberOfColors = bih->numberOfColors;
-    importantColors = bih->importantColors;
-    fwrite(bfh->type,sizeof(uint16_t),1,stream);
-    fwrite(&sizeOfFile,sizeof(uint32_t),1,stream);
-    fwrite(&reserved,sizeof(uint32_t),1,stream);
-    fwrite(&offset,sizeof(uint32_t),1,stream);
-	fwrite(&headerSize,sizeof(uint32_t),1,stream);
-    fwrite(&wide,sizeof(int32_t),1,stream);
-    fwrite(&tall,sizeof(int32_t),1,stream);
-    fwrite(&planes,sizeof(uint16_t),1,stream);
-    fwrite(&bitsPerPixel,sizeof(uint32_t),1,stream);
-    fwrite(&compression,sizeof(uint32_t),1,stream);
-    fwrite(&sizeOfImage,sizeof(uint32_t),1,stream);
-    fwrite(&xPixelsPerMeter,sizeof(uint32_t),1,stream);
-    fwrite(&yPixelsPerMeter,sizeof(uint32_t),1,stream);
-    fwrite(&numberOfColors,sizeof(uint32_t),1,stream);
-    fwrite(&importantColors,sizeof(uint32_t),1,stream);
-    /*Palette*/
-    for(i = 0; i < 1022; i++) {
-		fwrite(&i,sizeof(uint8_t),1,stream);
-	}
-	/*Bitmap data*/
-	for(i = 1; i < tall+1; i++) { /*Reverse scan, reverse tall, but not wide*/
-		for(i2 = 0; i2 < wide; i2++) { /*We just replotting our shit back again!*/
-			hold = data[(i2+((tall-i)*wide))];
-			fwrite(&hold,sizeof(uint8_t),1,stream);
-		}
-    }
-	return;
 }
--- leaf.h
+++ leaf.h
@@ -9,7 +9,46 @@
 
 #include <dos.h>
 
-#include "key.h" /*Keyboard keys*/
+/*Keys*/
+/*Function keys*/
+#define KEY_F1 0x03b
+#define KEY_F2 0x03c
+#define KEY_F3 0x03d
+#define KEY_F4 0x03e
+#define KEY_F5 0x03f
+#define KEY_F6 0x040
+#define KEY_F7 0x041
+#define KEY_F8 0x042
+#define KEY_F9 0x043
+#define KEY_F10 0x044
+#define KEY_F11 0x0ff85
+#define KEY_F12 0x0ff86
+
+/*Right pad keys, not the numpad!*/
+#define KEY_INS 0x052
+#define KEY_DEL 0x053
+#define KEY_HM 0x047
+#define KEY_END 0x04f
+#define KEY_PGUP 0x049
+#define KEY_PGDWN 0x051
+
+/*Arrow keys*/
+#define KEY_UP 0x048
+#define KEY_DOWN 0x050
+#define KEY_RIGHT 0x04d
+#define KEY_LEFT 0x04b
+
+/*#ifdef __APPLE2__
+#define KEY_UP 0xb
+#define KEY_DOWN 0xa
+#define KEY_RIGHT 0x15
+#define KEY_LEFT 0x8
+#endif*/
+
+/*Shift, tab, etc*/
+#define KEY_TAB 0x9
+#define KEY_ENTER 0xd
+#define KEY_ESC 0x1b
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
@@ -52,7 +91,7 @@ struct image {
 
 static uint8_t far *vgaMemory = (uint8_t far *)0xA0000000L;
 static uint8_t far *textMemory = (uint8_t far *)0xB8000000L;
-static uint16_t *clock = (uint16_t far *)0x0000046C;
+static uint16_t far *clock = (uint16_t far *)0x0000046C;
 
 union REGS in,out;
 
